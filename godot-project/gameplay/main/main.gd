4f30afe9150739fc5cf432e99203da26be726fc2
Moisés <moisesjbc@gmail.com>
2023-04-15 14:01:33 +0100

    Add sprites

--- godot-project/gameplay/main/main.gd
+++ godot-project/gameplay/main/main.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
+var current_level = 0
 
+
+func _ready():
+	set_current_level(0)
+
+
+func set_current_level(new_level):
+	current_level = new_level
