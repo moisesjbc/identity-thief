ab84968d4667ace453b2f7b0541f251e895a63fc
Moisés <moisesjbc@gmail.com>
2023-04-15 12:04:43 +0100

    Add multiple dummy NPCs

--- godot-project/gameplay/npc/npc.gd
+++ godot-project/gameplay/npc/npc.gd
@@ -3,10 +3,19 @@ extends KinematicBody2D
 export var speed = 300
 var velocity = Vector2.ZERO
 
+var colors = [
+	Color.orange,
+	Color.green,
+	Color.blue,
+	Color.red
+]
+
 func _ready():
 	randomize()
+	
 	velocity.x = -50 + randi() % 100
 	velocity.y = -50 + randi() % 100
+	$sprite.modulate = colors[randi() % len(colors)]
 
 
 func _process(delta):
