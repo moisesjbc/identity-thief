66d1253aadd377cf1d9f44d71dfb6eb55a3719f9
Moisés <moisesjbc@gmail.com>
2023-04-15 11:34:59 +0100

    Add swap (dummy)

--- godot-project/gameplay/player/player.gd
+++ godot-project/gameplay/player/player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 
 export var speed = 500
-
+var can_swap = true
 
 func _process(delta):
 	var velocity = Vector2.ZERO
@@ -17,4 +17,23 @@ func _process(delta):
 	elif Input.is_action_pressed("ui_down"):
 		velocity.y = 1
 		
-	move_and_collide(speed * velocity * delta)
+	var collision = move_and_collide(speed * velocity * delta)
+	if collision and collision.collider.is_in_group("npc") and can_swap:
+		swap(collision.collider)
+		
+func swap(npc):
+	print("SWAP")
+	var npc_sprite = npc.get_node("sprite")
+	var player_sprite = get_node("sprite")
+	
+	can_swap = false
+	$swap_timer.start()
+	npc.remove_child(npc_sprite)
+	remove_child(player_sprite)
+	
+	npc.add_child(player_sprite)
+	add_child(npc_sprite)
+
+
+func _on_timer_timeout():
+	can_swap = true
