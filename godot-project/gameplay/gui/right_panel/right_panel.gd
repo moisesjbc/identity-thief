9e1ecdd28148de08bad2080b6b5aef5d4a729f08
Moisés <moisesjbc@gmail.com>
2023-04-16 14:27:11 +0100

    Add styles to right panel

--- godot-project/gameplay/gui/right_panel/right_panel.gd
+++ godot-project/gameplay/gui/right_panel/right_panel.gd
@@ -2,5 +2,5 @@ extends Control
 
 
 func reset(current_level_index, target_identity_id):
-	$label.text = "Current level: " + str(current_level_index)
-	$margin_container/vbox_container/skeleton.set_identity_by_id(target_identity_id)
+	$banks_label.text = str(current_level_index)
+	$marco/skeleton.set_identity_by_id(target_identity_id)
