87a717a79848f5feaa29621c3279ff28d7f506b1
adriel87 <adrielarochaoronoz@gmail.com>
2023-04-16 09:32:56 +0100

    show score in game over

--- godot-project/GameOver.gd
+++ godot-project/GameOver.gd
@@ -8,7 +8,11 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$police_siren.play()
+	get_node("CenterContainer/TextureRect/LevelCount").text = str(get_tree().get_root().get_node("statistics").n_levels)
+	get_node("CenterContainer/TextureRect/SwapCount").text = str(get_tree().get_root().get_node("statistics").n_swaps)
+	print("n_levels " + str(get_tree().get_root().get_node("statistics").n_levels))
+	print("n_swaps " + str(get_tree().get_root().get_node("statistics").n_swaps))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
