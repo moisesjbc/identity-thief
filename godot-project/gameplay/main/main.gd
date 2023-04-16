0af9f46fd6f9a05328e6c7692595fd6c3b720ff8
Moisés <moisesjbc@gmail.com>
2023-04-16 13:27:26 +0100

    Add limit and debug prints

--- godot-project/gameplay/main/main.gd
+++ godot-project/gameplay/main/main.gd
@@ -8,15 +8,17 @@ var target_identity_id = 0
 
 ## Time
 export var time_initial_value = 15
-export var time_first_step_level = 4
+export var time_first_step_level = 0
 export var time_n_levels_per_step = 1
 export var time_step = -1
+export var time_limit = 10
 
 ## NPCs
 export var n_npcs_initial_value = 5
 export var n_npcs_first_step_level = 0
 export var n_npcs_n_levels_per_step = 1
 export var n_npcs_step = 1
+export var n_npcs_limit = 100
 
 var current_total_time = 15
 
@@ -24,12 +26,16 @@ func _ready():
 	set_current_level(0)
 	get_tree().get_root().get_node("music").play_gameplay_music()
 
-func compute_difficulty_value(current_level, initial_value, first_step_level, n_levels_per_step, step):
+func compute_difficulty_value(current_level, initial_value, first_step_level, n_levels_per_step, step, limit):
 	var difficulty_value = initial_value
 	
-	if current_level >= first_step_level:
+	print("---------------- initial : " + str(difficulty_value))
+	if current_level >= (first_step_level + 1):
 		difficulty_value = int(difficulty_value + step * ((current_level - (first_step_level + 1)) / n_levels_per_step))
-	
+	print("---------------- computed : " + str(difficulty_value))
+	if (step < 0 and difficulty_value < limit) or (step > 0 and difficulty_value > limit):
+		difficulty_value = limit
+	print("---------------- clamped : " + str(difficulty_value))
 	return difficulty_value
 
 func set_current_level(new_level):
@@ -40,7 +46,8 @@ func set_current_level(new_level):
 		time_initial_value,
 		time_first_step_level,
 		time_n_levels_per_step,
-		time_step
+		time_step,
+		time_limit
 	)
 	
 	var n_npcs = compute_difficulty_value(
@@ -48,7 +55,8 @@ func set_current_level(new_level):
 		n_npcs_initial_value,
 		n_npcs_first_step_level,
 		n_npcs_n_levels_per_step,
-		n_npcs_step
+		n_npcs_step,
+		n_npcs_limit
 	)
 	
 	print("SET level: " + str(current_level))
